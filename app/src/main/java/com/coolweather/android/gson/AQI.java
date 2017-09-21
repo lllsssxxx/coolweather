6e6b518fecb884db9447571f4360f0557045d064
lllsssxxx <627483908@qq.com>
2017-09-21 20:49:15 +0800

    显示天气信息

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by lllsssxxx on 2017/9/18.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String api;
+        public String pm25;
+    }
+}
---
+++ app/src/main/java/com/coolweather/android/gson/Basic.java
@@ -0,0 +1,21 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by lllsssxxx on 2017/9/18.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
